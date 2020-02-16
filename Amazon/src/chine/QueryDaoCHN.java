4f233b7839c8c7639ed7a65f6be52375c27b69a5
HP <HP@Salon>
2020-02-16 16:41:43 +0100

    optimisation classe dans QueryDaoCHN + ajout de nouvelle requêtes

--- Amazon/src/chine/QueryDaoCHN.java
+++ Amazon/src/chine/QueryDaoCHN.java
@@ -7,6 +7,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,7 +20,12 @@ import javax.swing.table.DefaultTableModel;
 	public class QueryDaoCHN {
 	
 		
-	private String line = "";
+	
+	private String csvFile = "Amazon_CHINE.csv";
+    private String line = "";
+    private String cvsSplitBy = ";";
+    private String[][] tabPaySlip = new String[250][250];
+    public int sizeTab;
 		
 	
 		public QueryDaoCHN() {
@@ -70,9 +77,7 @@ import javax.swing.table.DefaultTableModel;
 	
 	   
 	
-	        String csvFile = "Amazon_CHINE.csv";
-	        String line = "";
-	        String cvsSplitBy = ";";
+	       
 	
 	        try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
 	
@@ -93,11 +98,9 @@ import javax.swing.table.DefaultTableModel;
 	
 	}
 	public void recupCSV() {
-		String csvFile = "Amazon_CHINE.csv";
-	    String line = "";
-	    String cvsSplitBy = ";";
+		
 	    String payslip2[][] = new String [250][250];
-	    int i = 0;
+	    sizeTab = 0;
 	  
 	    
 	    try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
@@ -116,27 +119,56 @@ import javax.swing.table.DefaultTableModel;
 		            int j=0;
 		            
 		            while(j != 7) {
-		            	payslip2[i][j]=payslip[j];
+		            	payslip2[sizeTab][j]=payslip[j];
 		            	j++;
 		            }
-		            i++;
+		            sizeTab++;
 				}
-				for(int k=0; k<202; k++) {
-					for(int l=0; l<=6; l++) {
-						//System.out.println(payslip2[k][l]);
-					}
-					//System.out.println("\n");
-				}
+				
 				System.out.println(payslip2[4][1]);
 	    } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	public String[][] RecupTab() {
+		sizeTab = 0;
+		try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+		   	 try {
+				reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}/*On lit la première ligne car c'est les infos des colonnes*/
+		        
+				
+				while ((line = reader.readLine()) != null) {/*On lit toutes les lignes de notre fichier*/
+	
+		           //On stocke dans notre tableau nos différents éléments graçe à notre séparateur
+		            String[]payslip = line.split(cvsSplitBy);
+		            int j=0;
+		            
+		            while(j != 7) {
+		            	tabPaySlip[sizeTab][j]=payslip[j];
+		            	j++;
+		            }
+		            sizeTab++;
+				}
+		
+		
+	} catch (FileNotFoundException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+		System.out.println(tabPaySlip[0][1]);
+		return tabPaySlip;
+		
+}
+	/*Requete 1*/
 	public void highterSalarya() {/*On récupère le plus haut salaire*/
-		 String csvFile = "Amazon_CHINE.csv";
-	     String line = "";
-	     String cvsSplitBy = ";";
 	     int max = 0;
 	     int cpt = 0;
 	     //int hightsalary[] = null ;
@@ -298,7 +330,7 @@ import javax.swing.table.DefaultTableModel;
 		
 		
 	}
-	
+	/*Requête 2*/
 	public void sumPaidLeave() {
 		
 		String csvFile = "Amazon_CHINE.csv";
@@ -334,214 +366,147 @@ import javax.swing.table.DefaultTableModel;
 				}
 		
 	}
-	
+	/*Requête 3*/
 	public void salarywithpaidLeave() {
-		String csvFile = "Amazon_CHINE.csv";
-	    String line = "";
-	    String cvsSplitBy = ";";
-	    String[] name = new String[250];
-	    int[] salary = new int [250];
-	    int []hg = new int [250];
-	    int paidleave;
-	    int indice=0,cpt=0;
-	    int[] highterPaidLeave = new int [20];
-	    String firstname = null;
-	    int salarys = 0;
 	    
-		try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
-		   	 reader.readLine();/*On lit la première ligne car c'est les infos des colonnes*/
-		        
-				
-				while ((line = reader.readLine()) != null) {/*On lit toutes les lignes de notre fichier*/
-	
-		           //On stocke dans notre tableau nos différents éléments graçe à notre séparateur
-		            String[]payslip = line.split(cvsSplitBy);
-		            paidleave = Integer.parseInt(payslip[6]);//les congés
-		            
-					
-		          
-					name[indice]=payslip[2];//remplir les noms avec leur indice
-					hg[indice] = Integer.parseInt(payslip[6]);//remplit les congés avec leur indices
-		            salary[indice]=Integer.parseInt(payslip[4]);//remplit les salires avec leur indices
-		            
-		            indice++;
-		            //On recherche les plus gros congés
-		          if(payslip[6]!=" conges") {
-			             //converse Sting tab in Int tab 
-			             highterPaidLeave [cpt] = Integer.parseInt(payslip[6]);;//
-			             Arrays.sort(highterPaidLeave);// sort tab 
-			            
-		            }              
-		            
-		            
-			            
-		            }
-		        for(int i = highterPaidLeave.length-1; i>=highterPaidLeave.length-5; i--) {
-		          	 for(int j=0; j < salary.length; j++) {
-		          		 if(highterPaidLeave[i]==hg[j]) {
-		          			 firstname = name[j];
-		          			 salarys = salary[j];
-		          		 } 
-		          	}
-		          	 System.out.println("Nom : "+ firstname +"\tsalaire : " + salarys + "\tcongés :" + highterPaidLeave[i]);
-		        }
-	
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	}
-	
-	public void salaryBonus() {
-		String csvFile = "Amazon_CHINE.csv";
-	    String line = "";
-	    String cvsSplitBy = ";";
-	    String[] name = new String[250];
-	    int[] salary = new int [250];
-	    int []hg = new int [250];
-	    int bonus;
-	    int salaire;
-	    int indice=0,cpt=0;
-	    int[] highterSalary = new int [250];
-	    String firstname = null;
-	    int salarys = 0;
 	    
-		try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
-		   	 try {
-				reader.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}/*On lit la première ligne car c'est les infos des colonnes*/
-		        
-				
-				while ((line = reader.readLine()) != null) {/*On lit toutes les lignes de notre fichier*/
-	
-		           //On stocke dans notre tableau nos différents éléments graçe à notre séparateur
-		            String[]payslip = line.split(cvsSplitBy);
-		            bonus = Integer.parseInt(payslip[5]);//les congés
-		            salaire = Integer.parseInt(payslip[4]);
-		            highterSalary[indice] = salaire +(salaire * bonus)/100;
-		            salary[indice] = salaire +(salaire * bonus)/100;
-		            name[indice]= payslip[2];
-		            indice++;
+	    int paidLeave = 0, paidLeaveMax = 0;
+	    int salary = 0, salaryMax = 0;
+	    int tmp = 0;
+	    int[] tabPaidLeave = new int[250];
+	    int[] id = new int[250];
+	    HashMap<Integer,Integer> mapPaidLeave = new HashMap<Integer,Integer>();
+	    RecupTab();
+	    for(int k=0; k<sizeTab; k++) {/*we shearch the smallest paidleave*/
+	    	paidLeave = Integer.parseInt(tabPaySlip[k][6]);
+	    	if(paidLeave > paidLeaveMax) {
+	    		paidLeaveMax = paidLeave;
+	    	}
+	    }
+	    for(int j=0; j<sizeTab; j++) {/*we shearch the smallest paidleave*/
+			salary = Integer.parseInt(tabPaySlip[j][4]);
+			paidLeave = Integer.parseInt(tabPaySlip[j][6]);
+			if(paidLeave == paidLeaveMax) {
+				if(salary > salaryMax) {
+					id[tmp] = j;
+					tabPaidLeave[tmp] = salary;
+					salaryMax = salary;
+					tmp++;
 				}
-				Arrays.sort(highterSalary);
-				  for(int i = highterSalary.length-1; i>=highterSalary.length-3; i--) {//on read the ten highter salarys 
-			          	 for(int j=0; j < salary.length; j++) {
-			          		 if(highterSalary[i]==salary[j]) {
-			          			 firstname = name[j];
-			          			 
-			          		 } 
-			          	}
-			          	 System.out.println("Nom : "+ firstname +"\tsalaire : " + highterSalary[i] );
-			        }
-				
-				
-				
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	}
-	public void meduimSalary() {
-		 String csvFile = "Amazon_CHINE.csv";
-	    String line = "";
-	    String cvsSplitBy = ";";
-	    int computer = 0;
-	    int sommeSalary=0;
-	    int meduimSalary=0;
-		try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
-	  	 reader.readLine();/*On lit la première ligne car c'est les infos des colonnes*/
-	       while ((line = reader.readLine()) != null) {/*On lit toutes les lignes de notre fichier*/
-	
-	          //On stocke dans notre tableau nos différents éléments graçe à notre séparateur
-	           String[]payslip = line.split(cvsSplitBy);
-	           sommeSalary = sommeSalary + Integer.parseInt(payslip[4]);
-	           computer ++;
-	              
-	          
-	
 			}
-	       meduimSalary = sommeSalary/(computer);
+		}           
+		for(int i=tmp-1; i>tmp-6; i--) {
+			/*we post the 5 biggest salary with max paid leave */
+			System.out.println("salary with paidLeave : " + tabPaySlip[id[i]][0] + " salaire :" + tabPaySlip[id[i]][4]);
+		}
+	
+	}
+	/*Requête 4 */
+	public void salaryBonus() {	    
+	    int salary = 0, salaryBonus = 0;
+	    int bonus = 0;
+	    int[] id = new int[250];
+	    int[] tabSalaryBonus = new int[250];
+	    int i = 0, salaryMax = 0;
+	    HashMap<Integer,Integer> mapSalaryBonus= new HashMap<Integer,Integer>(); 
+	    RecupTab();
+	    
+			for(int k=0; k<sizeTab; k++) {/*we shearch the smallest paidleave*/
+				salary = Integer.parseInt(tabPaySlip[k][4]);
+				bonus = Integer.parseInt(tabPaySlip[k][5]);
+				salaryBonus = salary +(salary * bonus)/100;/*We add bonus in percent*/
+				mapSalaryBonus.put(k, salaryBonus);
+			}
+			for (Map.Entry mapentry : mapSalaryBonus.entrySet()) {
+				salary = (int) mapentry.getValue();
+				if(salary>salaryMax) {/*We put best salary with bonus in tab and the id correspondding*/
+					id[i] = (int) mapentry.getKey();
+					tabSalaryBonus[i] = (int) mapentry.getValue();
+					salaryMax = (int) mapentry.getValue();
+					i++;
+				}
+			}		
+			for(int j=i-1; j>i-4; j--) {
+				/*We post the 3 best salary with bonus include */
+				System.out.println(tabPaySlip[id[j]][1] + " : salaire avec bonus: "+tabSalaryBonus[j]);
+			}
+	}
+	/*Requête 5*/
+	public void meduimSalary() {
+	    int cmp = 0;
+	    int sommeSalary=0, salary = 0;
+	    int meduimSalary=0;
+	    RecupTab();
+	    
+		for(int k=0; k<sizeTab; k++) {/*we shearch the smallest paidleave*/
+			salary = Integer.parseInt(tabPaySlip[k][4]);
+			sommeSalary = sommeSalary + salary;
+			cmp ++;
+		}
+	          
+	       meduimSalary = sommeSalary/(cmp);
 	       System.out.println(meduimSalary);
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	
 	}
-	}
+	/*Requête 6*/
 	public void bestEmployees() {
-		String csvFile = "Amazon_CHINE.csv";
-	    String line = "";
-	    String cvsSplitBy = ";";
-	    String payslip2[][] = new String [250][250];
-	    int i = 0;
 	    int paidleaveMin=30, paidleave = 0;
 		int	bonusMax = 0, bonus = 0;
 		int salaryMax = 0, salary = 0;
 		int tmp = 0;
+		RecupTab();
 	    
-	    try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
-		   	 try {
-				reader.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}/*On lit la première ligne car c'est les infos des colonnes*/
-		        
-				
-				while ((line = reader.readLine()) != null) {/*On lit toutes les lignes de notre fichier*/
-	
-		           //On stocke dans notre tableau nos différents éléments graçe à notre séparateur
-		            String[]payslip = line.split(cvsSplitBy);
-		            int j=0;
-		            
-		            while(j != 7) {
-		            	payslip2[i][j]=payslip[j];
-		            	j++;
-		            }
-		            i++;
-				}
-				for(int k=0; k<i; k++) {/*we shearch the smallest paidleave*/
-					paidleave = Integer.parseInt(payslip2[k][6]);
+				for(int k=0; k<sizeTab; k++) {/*we shearch the smallest paidleave*/
+					paidleave = Integer.parseInt(tabPaySlip[k][6]);
 					if (paidleave < paidleaveMin) {
 						paidleaveMin = paidleave;
 					}
 				}
 				
-				for(int m=0; m<i; m++) {/*we shearch the biggest bonus among the smallest paidleave*/
-					bonus = Integer.parseInt(payslip2[m][5]);
-					if(Integer.parseInt(payslip2[m][6])==paidleaveMin) {
+				for(int m=0; m<sizeTab; m++) {/*we shearch the biggest bonus among the smallest paidleave*/
+					bonus = Integer.parseInt(tabPaySlip[m][5]);
+					if(Integer.parseInt(tabPaySlip[m][6])==paidleaveMin) {
 						if(bonus > bonusMax) {
 							bonusMax = bonus;
 						}
 					}
 				}
 				
-				for(int l=0; l<i; l++) {/*we shearch the biggest salary among the biggest bonus and the smallest paidleave*/
-					if((Integer.parseInt(payslip2[l][5])==bonusMax) &&(Integer.parseInt(payslip2[l][6])==paidleaveMin)) {
-						salary = Integer.parseInt(payslip2[l][4]);
+				for(int l=0; l<sizeTab; l++) {/*we shearch the biggest salary among the biggest bonus and the smallest paidleave*/
+					if((Integer.parseInt(tabPaySlip[l][5])==bonusMax) &&(Integer.parseInt(tabPaySlip[l][6])==paidleaveMin)) {
+						salary = Integer.parseInt(tabPaySlip[l][4]);
 						if(salary > salaryMax) {
 							salaryMax = salary;
 							tmp = l;
 						}
 					}
 				}
-				System.out.println(payslip2[tmp][0]);
-	    } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				System.out.println(tabPaySlip[tmp][2]);
+	    
+}
+	public void minAgeSalary() {
+	int minAge = 50, age = 0;
+	int maxSalary = 0, salary = 0;
+	int tmp = 0;
+	RecupTab();
+		for(int i=0; i<sizeTab; i++) {/*we shearch the smallest age*/
+			age = Integer.parseInt(tabPaySlip[i][3]);
+			if (age < minAge) {
+				minAge = age;
+			}
+		}
+		for(int j=0; j<sizeTab; j++) {/*we shearch the biggest salary*/
+			salary = Integer.parseInt(tabPaySlip[j][4]);
+			if(Integer.parseInt(tabPaySlip[j][3])==minAge) {
+			if (salary > maxSalary) {
+				maxSalary = salary;
+				tmp = j;
+			}
 		}
 	}
+		System.out.println("-------"+ tabPaySlip[tmp][1]);
+		System.out.println(tabPaySlip[tmp][2]);
+}
 	public void ajouter(PaySlipCHN payslip) {
 		// TODO Auto-generated method stub
 		
---
+++ Amazon/src/chine/TestChine.java
@@ -0,0 +1,15 @@
+package chine;
+
+public class TestChine {
+
+	public static void main(String[] args) {
+		QueryDaoCHN chine = new QueryDaoCHN();
+		chine.bestEmployees();
+		chine.minAgeSalary();
+		chine.meduimSalary();
+		chine.salaryBonus();
+		chine.salarywithpaidLeave();
+
+	}
+
+}
