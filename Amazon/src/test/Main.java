773bd29bc22357b950f99ea0b53a3a67ba007c6a
HP <HP@Salon>
2020-02-21 15:36:24 +0100

    modif classe QueryDaoCHN QueryDaoUSA (modif method query 8)

--- Amazon/src/test/Main.java
+++ Amazon/src/test/Main.java
@@ -3,11 +3,14 @@ package test;
 import amazon.QueryAmazon;
 import gui.Display;
 import query.QueryDaoFR;
+import query.QueryDaoUSA;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new Display();
+		//new Display();
+		//QueryDaoUSA usa = new QueryDaoUSA();
+		QueryDaoUSA.query8();
 	}
 
 }
