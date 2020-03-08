4d5ea93d27b0bd34a20f2c69d9acc4895d2bb787
HP <HP@Salon>
2020-03-08 20:38:48 +0100

    javadoc

--- Amazon/src/data/Convert.java
+++ Amazon/src/data/Convert.java
@@ -2,20 +2,39 @@ package data;
 
 import gui.ConverterPanel;
 
+/**
+ * 
+ * @author Ben Mansour Fares, Chahboun Taha, Duliege Clément
+ *
+ */
 public class Convert {
 	
+	/**
+	 * @param double
+	 * 		the salary to convert
+	 * @return the salary in Yen convert in Dollars.
+	 */
 	public static int convertIntCHNtoUSA(double salary) {
 		double quotient = 0.47;
 		salary*=quotient;
 		return (int)salary;
 		
 	}
+	/**
+	 *  @param double
+	 * 		the salary to convert
+	 * @return the salary in Euro convert in Dollars.
+	 */
 	public static int convertIntFRtoUSA(double salary) {
 		double quotient = 0.89;
 		salary/=quotient;
 		return (int)salary;
 	}
-	
+	/**
+	 *  @param String
+	 * 		the salary to convert
+	 * @return the salary in Yen convert in Dollars.
+	 */
 	public static String convertStringCHNtoUSA(String value) {
 		double quotient = 0.47;
 		double salary = Double.parseDouble(value);
@@ -25,6 +44,11 @@ public class Convert {
 		return strSalary;
 		
 	}
+	/**
+	 *  @param double
+	 * 		the salary to convert
+	 * @return the salary in Euro convert in Dollars.
+	 */
 	public static String convertStringFRtoUSA(String value) {
 		double quotient = 0.89;
 		double salary = Double.parseDouble(value);
@@ -33,7 +57,9 @@ public class Convert {
 		String strSalary = Integer.toString(intSalary);
 		return strSalary;
 	}
-	
+	/**
+	 * Method allowing to convert Euro in dollars or yen 
+	 */
 	public void convertEuros() {
 		double quotientdollars=0.89;
 		double quotientyen=0.418;
@@ -43,7 +69,9 @@ public class Convert {
 		ConverterPanel.getConvert().setText("<html>"+ dollars+" Dollars <br/>" +yen+" Yen </html>");
 	
 	}
-	
+	/**
+	 * Method allowing to convert Dollars in euro or yen 
+	 */
 	public void convertDollars() {
 		double quotienteuros=0.89;
 		double quotientyen=0.47;
@@ -53,7 +81,9 @@ public class Convert {
 		ConverterPanel.getConvert().setText("<html>"+ euros+" Euros <br/>" +yen+" Yen </html>");
 	
 	}
-	
+	/**
+	 * Method allowing to convert Yen in dollars or Euro 
+	 */
 	public void convertYen() {
 		double quotientdollars=0.47;
 		double quotienteuros=0.418;
